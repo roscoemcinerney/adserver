c635567f6c3fdf865fb6c2c973f24e28f8540803
Daniel Winterstein <daniel@sodash.com>
2017-04-04 15:25:16 +0100

    leaderboard sketch

--- adunit/dummy-init.js
+++ adunit/dummy-init.js
@@ -14,11 +14,12 @@ goodloop.vert = {
 	"mobileVideo":"https://as.good-loop.com/vert/kerb-288p.m4v",
 	"name":"Kerb",
 	"url":"http://kerbfood.com",
+	"logo": "ad-logo@2x.png",
 	"id":"egpub good loop com yhxz"
 };
 
 goodloop.charities = [
-	{"logo":"https://logo.clearbit.com/shelter.org.uk","name":"Shelter","url":"http://shelter.org.uk","id":"shelter"},
+	{"logo":"https://logo.clearbit.com/shelter.org.uk","name":"Shelter","url":"http://shelter.org.uk","id":"shelter", "photo":"image_shelter.jpg"},
 	{"logo":"http://as.good-loop.com/vert/alzheimers_research.png","name":"Alzheimer\u0027s Research","url":"http://www.alzheimersresearchuk.org/","id":"alzheimers"},
 	{"logo":"http://as.good-loop.com/vert/Battersea_Dogs_\u0026_Cats_Home_logo.png","name":"Battersea Dogs \u0026 Cats Home","url":"https://www.battersea.org.uk/","id":"battersea-dogs"},
 ];
