77df8d826c9df6f36cdf43a5998fa58f8265928b
DA <daniel.appel.winterwell@gmail.com>
2017-04-20 11:16:13 +0100

    added scripts to quickly update the templates.good-loop.com and showcase.good-loop.com sites

---
+++ update-showcase.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+##update showcase.good-loop.com
+
+
+scp showcase.html winterwell@hugh.soda.sh:/home/winterwell/showcase.good-loop.com/
+echo "showcase.html sent to server"
---
+++ update-templates.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+## update templates.good-loop.com
+
+scp templates.html winterwell@hugh.soda.sh:/home/winterwell/templates.good-loop.com/
+echo "templates.html sent to server"
